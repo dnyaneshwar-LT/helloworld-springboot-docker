b0579f0251f05106873684e5e1de116db82af529
EC2 Default User <ec2-user@ip-172-31-43-132.ap-south-1.compute.internal>
2020-04-19 12:47:06 +0000

    change in code

--- src/main/java/ch/hsr/cloud/docker/helloworld/Application.java
+++ src/main/java/ch/hsr/cloud/docker/helloworld/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker Cloud World!";
+		return "Hello springboot docker Cloud World!";
 	}
 
 	public static void main(String[] args) {
